0ef8510783376ccf2fb3f0a9f115ea6bd3a1bf67
Alexey Konovalov <aurulen@mail.ru>
2024-01-15 22:36:20 +0300

    Делаю БД

--- Simple_voting/Landing/views.py
+++ Simple_voting/Landing/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 
+from forms import UserForm
+from models import User
 
 def default_menu() -> tuple:
     return (
@@ -15,6 +17,14 @@ def index_page(request: HttpRequest) -> HttpResponse:
     context = {'page_name': 'Главная', 'menu': default_menu()}
     return render(request, 'index.html', context)
 
+def register(request):
+    if(request.method == 'POST'):
+        f = UserForm(request.POST)
+        if f.is_valid():
+            a = f.data['login']
+            b = f.data['password']
+            u = User(login=a, password=b)
+            u.save()
 
 
 
