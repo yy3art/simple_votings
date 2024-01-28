dd720e08989b50588ad0ab7ce79f6c340b7dec48
Ваше Имя <you@example.com>
2024-01-28 20:38:41 +0300

    разобрались с бд и добавили одно тестовое гс, помогал Михаил Помпушко

--- Simple_voting/Landing/models.py
+++ Simple_voting/Landing/models.py
@@ -1,7 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+# models.CharField
+# models.IntegerField
+# models.FloatField
 
 
-class Voiting(models.Model):
-    name = models.CharField(max_length=200)
+class Voting(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name_gl = models.CharField(max_length=200)
+    opis_gl = models.CharField(max_length=2000)
+    ans_1 = models.CharField(max_length=100)
+    ans_2 = models.CharField(max_length=100)
+    question = models.CharField(max_length=1000)
+    count_1 = models.IntegerField()
+    count_2 = models.IntegerField()
+    count_all = models.IntegerField()
+    persent_1 = models.FloatField()
+    persent_2 = models.FloatField()
+
+
 
 # Create your models here.
