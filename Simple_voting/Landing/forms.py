9bed9f43966bcaf3f6088f44fa891b7164dd8fb9
Vyacheslav Smirnov <smslava7@gmail.com>
2024-01-31 22:23:46 +0300

    REsolving conflicts

---
+++ Simple_voting/Landing/forms.py
@@ -0,0 +1,42 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+class UserRegistrationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+class LoginForm(forms.Form):
+    username = forms.CharField(
+        max_length=20,
+        required=True,
+        widget=forms.TextInput(
+            attrs={
+                'placeholder': 'Username',
+            }
+        )
+    )
+
+    password = forms.CharField(
+        max_length=20,
+        required=True,
+        widget=forms.PasswordInput(
+            attrs={
+                'placeholder': 'Password',
+            }
+        )
+    )
+class CreateVotingForm(forms.Form):
+    question = forms.CharField(
+        label='Вопрос',
+        required=True
+    )
+    answer1 = forms.CharField(
+        label='Ответ 1',
+        required=True
+    )
+    answer2 = forms.CharField(
+        label='Ответ 2',
+        required=True
+    )
--- Simple_voting/Simple_voting/settings.py
+++ Simple_voting/Simple_voting/settings.py
@@ -100,6 +100,11 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+LOGIN_URL = '/login/'
+
+LOGIN_REDIRECT_URL = '/'
+
+LOGOUT_REDIRECT_URL = '/'
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
