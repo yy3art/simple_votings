d9c64bbca786c9dc1e3d214536600e1292a9ffa5
Александр Козленко <aleks.den.ko@yandex.ru>
2024-02-02 16:47:24 +0000

    CreateVotingForm

--- Simple_voting/Landing/forms.py
+++ Simple_voting/Landing/forms.py
@@ -27,4 +27,18 @@ class LoginForm(forms.Form):
             }
         )
     )
->>>>>>> main
+
+
+class CreateVotingForm(forms.Form):
+    question = forms.CharField(
+        label='Вопрос',
+        required=True
+    )
+    answer1 = forms.CharField(
+        label='Ответ 1',
+        required=True
+    )
+    answer2 = forms.CharField(
+        label='Ответ 2',
+        required=True
+    )
